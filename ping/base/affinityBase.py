35950181093e31dda94676f52597c8bf93749117
Mark Moore <mark@moore.dk>
2018-06-27 14:50:40 +0200

    base functionality imlemented

--- ping/base/affinityBase.py
+++ ping/base/affinityBase.py
@@ -2,21 +2,32 @@
 
 from IPy import IP
 import socket
-from time import time, ctime
+from time import time, ctime, sleep
+
+from BroadcasterWebsocketServer import BroadcasterWebsocketServer
+
+
 import struct
 import json
+import threading, Queue
 
+
+
+wsport = 5000
 incomingPort = 5005
+distanceTimeout=2
 
 UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 listen_addr = ("",incomingPort)
 UDPSock.bind(listen_addr)
 
-DISTANCEFORMAT = 'cI' #Char, unsigned Int
+DISTANCEFORMAT = 'cH' #Char, unsigned short - TODO: looks to be expecting 8 bytes...  Is this Arduino compatible?
 DRAWFORMAT     = 'cII'
-RELEASEFORMAT  = 'cc'
 DRAW_EVENT_SIZE = struct.calcsize(DRAWFORMAT)
 DISTANCE_EVENT_SIZE = struct.calcsize(DISTANCEFORMAT)
+
+print 'distance_event_size:' + str(DISTANCE_EVENT_SIZE)
+print 'draw_event_size:' + str(DRAW_EVENT_SIZE)
 
 
 #ideal data format for a post-it note:
@@ -26,90 +37,219 @@ DISTANCE_EVENT_SIZE = struct.calcsize(DISTANCEFORMAT)
 # D - Distance in mm from sensor D
 # X - Collection of x,y coordinates that constitute a payload for picture data
 
-distances={} #dict
-Xpoints=[] #set
-Ypoints=[] #set
 
 
-class SetEncoder(json.JSONEncoder):
-    def default(self, obj):
-       if isinstance(obj, set):
-          return list(obj)
-       return json.JSONEncoder.default(self, obj)
-
-def postIt(distances, payload,timeStamp): #called when a post-it has 4 distance points and a collection of point data
-
-    pos = json.dumps({'pos':distances}) #<-works
 
 
-    img = json.dumps({'img':payload}, cls=SetEncoder)
+def postIt(distances, payload,timeStamp,postitQ): #called when a post-it has 4 distance points and a collection of point data
+
+    #pos = json.dumps({'pos':distances}) #<-works
+
+
+    #img = json.dumps({'img':payload}, cls=SetEncoder)
+    points = "{"
+    i=0
+    while i < len(payload):
+        set=iter(payload[i])
+        #points += "\"" + str(i) + "\": " + "{\"X\": "+ str(next(set)) + ", \"Y\": " + str(next(set)) + "}" #quick and dirty formatting...
+        points += "[" + str(next(set)) + "," + str(next(set)) + "]  " #quick and dirty formatting...
+        if(i<len(payload)-1) : points +=","
+        i += 1
+    points +="}"
 
     dt = json.dumps({'time':timeStamp})
 
-    package = '{\"postIt\":{' + dt + ',' + pos + img + '}}' #quick and dirty, because i couldn't be bothered with jsonmerge
+    package = '{' + "\"time\": " + str(timeStamp) + "," + "\"pos\": " + str(distances).replace('\'','\"') + "," + "\"img\": " + points + "}" #quick and dirty, because i couldn't be bothered with jsonmerge
 
     #print 'pos:' + pos
     #print 'img:' + img
     #print 'time:' + dt
 
-    print 'package: '
-    print package
-
-while True:
-
-    # Report on all data packets received and
-    # where they came from in each case (as this is UDP, each may be from a different source and it's up to the server to sort this out!)
-
-    # recvfrom waits for incoming data:
-    data, addr = UDPSock.recvfrom(5005) #Data is prefixed with type:  A,B,C and D are distance measurements, X is touch panel data
+    print 'Packaged post-it. Sending to queue'
+    #print package
+    postitQ.put(package)
+    # TODO: Serve the data through a websocket to whomever is listening...
 
 
-    remoteIP = IP(addr[0]).strNormal()  # convert address of packet origin to string
+def udpThread(dataQ):
+    print 'udp thread started.'
 
-    print (str(ctime()) + ': RX: \"' + str(data.rstrip('\n')) + '\" from ' + str(remoteIP)) + "size: " + str(len(data)) + " bytes total"
+    while True:
 
-    print
-    print ' RX: "%s" @ %s from %s' % (data.rstrip('\n'), ctime(), remoteIP)
-    print
+        # Report on all data packets received and
+        # where they came from in each case (as this is UDP, each may be from a different source and it's up to the server to sort this out!)
 
-    # poorly constructed data validation.. bear with me...
+        # recvfrom waits for incoming data:
+        data, addr = UDPSock.recvfrom(incomingPort) #Data is prefixed with type:  A,B,C and D are distance measurements, X is touch panel data
 
-    if (data[0]=='X' or data[0]=='Y') and len(data)==DRAW_EVENT_SIZE:
-        print 'Got Point Data'
-        (id, x,y) = struct.unpack(DRAWFORMAT, data)
 
-#        print id
-#        print x
-#        print y
+        remoteIP = IP(addr[0]).strNormal()  # convert address of packet origin to string
 
-        #Store in a collection that matches the ID.
-        #quick and dirty duplicate code.. sue me.
-        if id=='X':
-            Xpoints.append({x,y})
-            #print Xpoints
+        print ' RX: "%s" @ %s from %s' % (data.rstrip('\n'), ctime(), remoteIP)
 
-        if id=='Y':
-            Ypoints.append({x, y})
+        dataQ.put(data) #push data to other thread (via queue)
 
-    if (data[0]=='A' or data[0]=='B' or data[0]=='C' or data[0]=='D') and len(data)==DISTANCE_EVENT_SIZE:
-        print 'Got Distance Data'
-        (id,distance) = struct.unpack(DISTANCEFORMAT, data)
+def dataHandlingThread(dataQ,postitQ):
+    distances = {}  # dict
+    Xpoints = []  # set
+    Ypoints = []  # set
+    Zpoints = []  # set
+    Qpoints = []  # set
+
+    Xtriggered = False
+    Ytriggered = False
+    Ztriggered = False
+    Qtriggered = False
+
+    distance_data_RX_time = 0
+
+    print 'data handling thread started.'
+
+
+    while True:
+
+        data=[0,0]
+
+        try:
+            data = dataQ.get(False)  # get data from other thread (via queue)
+        except Queue.Empty:
+            pass
+
+
+        # poorly constructed data validation.. bear with me...
+        if(data[0]=='!'):
+            print 'got trigger data'
+
+            if data[1] == 'X':
+                Xtriggered=True
+            if data[1] == 'Y':
+                Ytriggered=True
+            if data[1] == 'Z':
+                Ztriggered=True
+            if data[1] == 'Q':
+                Qtriggered=True
+
+        if (data[0]=='X' or data[0]=='Y' or data[0]=='Z' or data[0]=='Q') and len(data)==DRAW_EVENT_SIZE:
+            (id, x,y) = struct.unpack(DRAWFORMAT, data)
+            print 'Got Point Data: x:' + str(x) + ' y:' + str(y)
 
     #        print id
-    #        print distance
+    #        print x
+    #        print y
 
-        distance_data_RX_time = int(time())  # Start a timer
+            #Store in a collection that matches the ID.
+            #quick and dirty duplicate code.. sue me.
+            if id=='X':
+                Xpoints.append({x,y})
+                #print Xpoints
+            if id=='Y':
+                Ypoints.append({x,y})
+            if id=='Z':
+                Zpoints.append({x,y})
+            if id=='Q':
+                Qpoints.append({x,y})
 
-        distances[id]=distance
+        if (data[0]=='A' or data[0]=='B' or data[0]=='C' or data[0]=='D') and len(data)==DISTANCE_EVENT_SIZE:
+            print 'Got Distance Data'
 
-        print 'Distances:' + str(distances)
-
-        if len(distances)==4:
-            postIt(distances,Xpoints,distance_data_RX_time)
-
-    #if $SOMETIME has passed while not receiving 4 distance measurements, cut it short and release it as an incomplete set.
+    #        print format(ord(data[0]), 'b')
+    #        print format(ord(data[1]), 'b')
+    #        print format(ord(data[2]), 'b')
+    #        print format(ord(data[3]), 'b')
 
 
+            id=data[0]
+
+            distance=ord(data[1])<<8 | ord(data[2]) #manual unpack, since debugging the python unpack took too long.
+
+            #(id,distance) = struct.unpack(DISTANCEFORMAT, data)
+
+        #        print id
+        #        print distance
+
+            distance_data_RX_time = int(time())  # Start a timer
+
+            #print(distance_data_RX_time)
+
+            distances[id]=distance
+
+            print 'Distances:' + str(distances)
+
+            #if nothing is triggered ignore distance:
+            #TODO: this is where one would implement functionality of moving already placed post-its.
+            if not (Xtriggered==True or Ytriggered==True or Ztriggered==True or Qtriggered==True):
+                print "no post-its are triggered, ignoring distance data."
+                distances = {}  # clear the distances dict
+
+
+    # if $SOMETIME has passed while not receiving 4 distance measurements, cut it short and release it as an incomplete set.
+                # TODO: The timeout does not fire!
+        if (Xtriggered==True or Ytriggered==True or Ztriggered==True or Qtriggered==True) and len(distances)==4 or (len(distances)>0 and (int(time())>distance_data_RX_time+distanceTimeout)):
+
+            # Clear the relevant points set:
+            if Xtriggered == True:
+                postIt(distances, Xpoints, distance_data_RX_time,postitQ)
+                Xpoints = []
+                Xtriggered = False
+
+            if Ytriggered == True:
+                postIt(distances, Ypoints, distance_data_RX_time,postitQ)
+                Ypoints = []
+                Ytriggered = False
+
+            if Ztriggered == True:
+                postIt(distances, Zpoints, distance_data_RX_time,postitQ)
+                Zpoints = []
+                Ztriggered = False
+
+            if Qtriggered == True:
+                postIt(distances, Qpoints, distance_data_RX_time,postitQ)
+                Qpoints = []
+                Qtriggered = False
+
+            distances = {}  # clear the distances dict
+
+
+
+
+
+def websocketThread(postitQ):
+
+
+    print 'websocket thread started.'
+    server = BroadcasterWebsocketServer('', wsport, True)
+    server.start()
+
+    print 'websocket server started'
+
+    while True:
+
+        try:
+            postit = postitQ.get(False)  # get data from other thread (via queue) - non blocking
+            print "got postit data:" + postit
+            server.broadcast(postit)
+        except Queue.Empty:
+            pass
+
+
+dataQ = Queue.Queue()
+postitQ = Queue.Queue()
+
+thread1 = threading.Thread(target=udpThread,args=(dataQ,))
+thread2 = threading.Thread(target=dataHandlingThread,args=(dataQ,postitQ,))
+thread3 = threading.Thread(target=websocketThread,args=(postitQ,))
+
+#make threads killable with ctrl+c:
+thread1.daemon=True
+thread2.daemon=True
+thread3.daemon=True
+
+thread1.start()
+thread2.start()
+thread3.start()
+
+while True:
+    sleep(1)
 
 
 
